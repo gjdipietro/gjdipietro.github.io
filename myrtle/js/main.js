882dc78fd2f270cb4982c472bb942e6e907c84f9
Greg DiPietro <gjdipietro@gmail.com>
2014-06-15 10:19:17 -0400

    Added a few pictures

--- myrtle/js/main.js
+++ myrtle/js/main.js
@@ -25,171 +25,126 @@
 		{
 			caption: 'Come on in!',
 			image: [
-		      "build/img/beach11.jpg",
-		      "build/img/beach11-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
+		      "build/img/beach1.jpg",
 		    ],
 		},
-		{
-			caption: 'Living Room, with updated furniture.',
-			image: [
-		      "build/img/beach3.jpg",
-		      "build/img/beach3-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
-		    ],
-		},
-
 		{
 			caption: 'Dining Room with island, great for entertaining.',
 			image: [
-		      "build/img/beach4.jpg",
-		      "build/img/beach4-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
+		      "build/img/beach2.jpg",
 		    ],
 		},
 
 		{
-			caption: 'Dining Room looking into the living room.',
+			caption: 'A view of the living room from the dining room table.',
+			image: [
+		      "build/img/beach3.jpg",
+		    ],
+		},
+
+		{
+			caption: 'The master bedroom.',
+			image: [
+		      "build/img/beach4.jpg",
+		    ],
+		},
+
+		{
+			caption: 'Laundry and bathroom attached to the master bedroom.',
 			image: [
 		      "build/img/beach5.jpg",
-		      "build/img/beach5-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
 		    ],
 		},
 
 		{
-			caption: 'Master bedroom.',
+			caption: 'Bathroom, towels are provied.',
 			image: [
 		      "build/img/beach6.jpg",
-		      "build/img/beach6-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
-		    ],
-		},
-
-		{
-			caption: 'Laundry and bathroom attached to master bedroom.',
-			image: [
-		      "build/img/beach7.jpg",
-		      "build/img/beach7-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
-		    ],
-		},
-
-		{
-			caption: 'Bathroom.',
-			image: [
-		      "build/img/beach8.jpg",
-		      "build/img/beach8-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
 		    ],
 		},
 
 		{
 			caption: 'The sunroom.  A great place to read the morning paper.',
 			image: [
-		      "build/img/beach9.jpg",
-		      "build/img/beach9-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "500",
-		    ],
-		},
-		{
-			caption: 'View of the breakfat nook, from the kitchen.',
-			image: [
-		      "build/img/beach17.jpg",
-		      "build/img/beach17-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
+		      "build/img/beach7.jpg",
 		    ],
 		},
 
 		{
-			caption: 'View from the sunroom to the dining room.',
+			caption: 'The sunroom looking out into the rest of the home.',
+			image: [
+		      "build/img/beach8.jpg",
+		   ],
+		},
+		{
+			caption: 'The cozy living room with updated furniture.',
+			image: [
+		      "build/img/beach9.jpg",
+		    ],
+		},
+
+		{
+			caption: 'View of the house from across the street.',
 			image: [
 		      "build/img/beach10.jpg",
-		      "build/img/beach10-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
 		    ],
 		},
 
 		
-
-		{
-			caption: 'Birds eye view.',
-			image: [
-		      "build/img/beach12.jpg",
-		      "build/img/beach12-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
-		    ],
-		},
 
 		{
 			caption: 'Just a few feet from the beach access.',
 			image: [
-		      "build/img/beach13.jpg",
+		      "build/img/beach11.jpg",
 		    ],
-		    sizes: [
-		      "1024",
-		      "320",
+		},
+
+		{
+			caption: 'Viewing the house coming back from the beach.  The outdoor shower is great to rinse off.',
+			image: [
+		      "build/img/beach12.jpg",
 		    ],
 		},
 
 		
 
 		{
-			caption: 'Viewing the house coming back from the beach.  The outdoor shower is great to rinse off.',
+			caption: 'The breakfast nook and entryway.',
 			image: [
-		      "build/img/beach15.jpg",
-		      "build/img/beach15-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
+		      "build/img/beach13.jpg",
 		    ],
 		},
 
 		{
-			caption: 'Air conditioned.',
+			caption: 'A bedroom.',
 			image: [
-		      "build/img/beach18.jpg",
-		      "build/img/beach18-sm.jpg",
-		    ],
-		    sizes: [
-		      "1024",
-		      "320",
+		      "build/img/beach14.jpg",
 		    ],
 		},	
+		{
+			caption: 'Big closet in the bedroom.',
+			image: [
+		      "build/img/beach15.jpg",
+		    ],
+		},
+		{
+			caption: 'Another bedroom with attached bathroom.',
+			image: [
+		      "build/img/beach16.jpg",
+		    ],
+		},
+		{
+			caption: 'Bedroom looking out into the kitchen area.',
+			image: [
+		      "build/img/beach17.jpg",
+		    ],
+		},
+		{
+			caption: 'Kitchen.',
+			image: [
+		      "build/img/beach18.jpg",
+		    ],
+		},
 	];
 	
 
